8657c38cf572985daf10fa028f158ca0b14f9157
Max Li <maximilianm.li@gmail.com>
2015-10-11 16:16:05 -0700

    add jm route and style

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
 app.use(express.static(__dirname));
-app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname+'/jamie'))
+
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/jm', function(req,res){
+  res.sendFile(path.join(__dirname+'/jamie/jamie.html'))
+})
+
+app.listen(process.env.PORT || 4000);
